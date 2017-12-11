481e36ccfb19e55abecbaf81e8536650d77f1064
M.C. van den Boogaart <scola@kvdb.name>
2017-12-11 14:11:01 +0100

    Fix empty check, do not auto merge

--- src/validator.js
+++ src/validator.js
@@ -27,15 +27,22 @@ export default class Validator extends Worker {
   }
 
   act(box, data, callback) {
-    data = this.filter(box, data);
+    const result = this.filter(box, data);
 
     this._structure
       .filter((section) => section.fields)
       .forEach((section) => {
-        this._validateFields(section.fields, data);
+        this._validateFields(section.fields, result);
       });
 
+    this.merge(box, data, result);
     this.pass(box, data, callback);
+  }
+
+  _isEmpty(value) {
+    return typeof value === 'undefined' ||
+      value === null ||
+      value === '';
   }
 
   _throwError(reason, field) {
@@ -46,10 +53,22 @@ export default class Validator extends Worker {
     throw error;
   }
 
+  _validateArray(field, value) {
+    if (Array.isArray(value) === false) {
+      return this._throwError('array', field);
+    }
+
+    for (let i = 0; i < value.length; i += 1) {
+      value[i] = this._validateType(field, value[i]);
+    }
+
+    return value;
+  }
+
   _validateField(field, data) {
     const value = data[field.name];
 
-    if (typeof value === 'undefined') {
+    if (this._isEmpty(value) === true) {
       if (field.required === true) {
         this._validateRequired(field, value);
       } else if (typeof field.default !== 'undefined') {
@@ -65,18 +84,6 @@ export default class Validator extends Worker {
     }
 
     data[field.name] = this._validateType(field, value);
-  }
-
-  _validateArray(field, value) {
-    if (Array.isArray(value) === false) {
-      return this._throwError('array', field);
-    }
-
-    for (let i = 0; i < value.length; i += 1) {
-      value[i] = this._validateType(field, value[i]);
-    }
-
-    return value;
   }
 
   _validateFields(fields, data) {
