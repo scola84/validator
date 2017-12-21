e7517b726fc21b9536d04f29af7a22f5996435c3
M.C. van den Boogaart <scola@kvdb.name>
2017-12-21 11:45:55 +0100

    Fix strings

--- src/locale/nl_NL.js
+++ src/locale/nl_NL.js
@@ -2,8 +2,8 @@ export default {
   error: {
     long: {
       400: {
-        empty: 'Het veld "%s" mag niet leeg zijn',
-        type: 'Het veld "%s" moet %s zijn'
+        empty: 'Het veld "%s" mag niet leeg zijn.',
+        type: 'Het veld "%s" moet %s zijn.'
       }
     }
   },
