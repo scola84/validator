02ac77a7ca43e3b2a0bd0eeb9d21a7ee52b0a940
M.C. van den Boogaart <scola@kvdb.name>
2018-01-07 23:23:02 +0100

    Update strings

--- src/locale/nl_NL.js
+++ src/locale/nl_NL.js
@@ -1,15 +1,18 @@
 export default {
-  error: {
-    long: {
-      400: {
-        empty: 'Het veld "%s" mag niet leeg zijn.',
-        type: 'Het veld "%s" moet %s zijn.'
-      }
-    }
-  },
   validator: {
-    email: 'een e-mailadres',
-    integer: 'een geheel getal',
-    text: 'een tekst'
+    error: {
+      long: {
+        400: {
+          checkbox: 'Selecteer minstens één %s.',
+          empty: 'Het veld "%s" mag niet leeg zijn.',
+          type: 'Het veld "%s" moet %s zijn.'
+        }
+      }
+    },
+    type: {
+      email: 'een e-mailadres',
+      integer: 'een geheel getal',
+      text: 'een tekst'
+    }
   }
 };
