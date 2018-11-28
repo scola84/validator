705c96db4cef6eaea3d7df17fd454d63978007e1
M.C. van den Boogaart <scola@kvdb.name>
2018-11-28 13:02:18 +0100

    Adjust tel checker

--- src/check/tel.js
+++ src/check/tel.js
@@ -1,26 +1,29 @@
 import {
-  formatNumber,
-  parseNumber
+  parsePhoneNumber
 } from 'libphonenumber-js';
 
 export default class TelCheck {
   check(field, value, data) {
+    try {
+      return this._check(field, value, data);
+    } catch (error) {
+      return false;
+    }
+  }
+
+  _check(field, value, data) {
     if (typeof value !== 'string') {
       return false;
     }
 
     const [, country = ''] = (data.locale || '').split('_');
 
-    const number = parseNumber(value, country, { extended: true });
+    const number = parsePhoneNumber(value, country);
 
-    if (typeof number.valid === 'undefined') {
+    if (number.isValid() === false && field.strict !== false) {
       return false;
     }
 
-    if (number.valid === false && field.strict !== false) {
-      return false;
-    }
-
-    return formatNumber(number, 'E.164');
+    return number.format('E.164');
   }
 }
